da1d9199f97ba34c9e1f032b49fed3fec73d2295
Jing <jingxia1219@gmail.com>
2018-10-04 13:20:03 -0700

    adfdsf

--- read_question.js
+++ read_question.js
@@ -3,12 +3,33 @@ const reader = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+
+function askName() {
 reader.question("What is your name?", function(answer) {
     console.log(`Hello ${answer}!`);
     reader.close(); //adding this line will close the program after it runs through the code
     //otherwise it will be waiting for your input even after the program reaches last line of code
 });
 console.log("Last program line");
+}
+
 // the purpose of this exercise is to demonstrate 
 // asynchronous, as Last program line will print before 
 // it logs hello ${answer} as it wont wait for the answer input
+
+function addTwoNumbers(callback) {
+    reader.question("Enter #1: ", function(string1){
+        reader.question("Enter #2: ", function(string2) {
+            const n1 = parseInt(string1); //answer is a string, need to convert
+            const n2 = parseInt(string2); // to interger 
+            callback(n1+n2);
+            reader.close();
+        });
+    });
+}
+
+askName();
+addTwoNumbers(function(result) {
+    console.log(result);
+
+});
