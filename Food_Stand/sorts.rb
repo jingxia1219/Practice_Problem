d33403c22e53167c13d853acb4005da6e2e32c70
Jing <jingxia1219@gmail.com>
2018-10-12 16:56:14 -0700

    added remove_duplicates

--- Food_Stand/sorts.rb
+++ Food_Stand/sorts.rb
@@ -35,9 +35,11 @@ def sort3(strings, length)
     i = length - 1 
     letters = ("a".."z").to_a
     while i >= 0 
+        # O(k)
         buckets = Array.new(26) {[]}
         strings.each do |string|
         buckets[letters.find_index(string[i])] << string 
+        # O(n)
         end 
         strings = buckets.flatten
         i-=1
@@ -50,3 +52,27 @@ sort3(['cat', 'car', 'bat'],3)
 # "car" "cat" "bat"
 # "bat" "car" 'cat'
 
+
+# Remove Duplicates from Sorted Array
+# Given a *SORTED* array, remove the duplicates *in-place* such that each element appear only once and return the new length.
+
+# Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+# Example
+# Input: [1, 1, 2]
+
+# Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+
+# It doesn't matter what you leave beyond the new length.
+
+def remove_duplicates(arr)
+    (arr.length - 1).times do |i|
+        next if arr[i] != arr[i+1]
+        arr.delete_at(i)
+    end 
+    p arr 
+end 
+
+remove_duplicates([1, 1, 2])
+remove_duplicates([3, 3, 4,5,5,6])
+# remove_duplicates([1, 1, 2])
